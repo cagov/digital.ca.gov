1d53a0673fe1d5b5d0be374d23d35ac7fc7df1d3
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-06-14 15:24:12 -0700

    Revert "innitial commit"
    This reverts commit f6a8a7f5e5bfa9cb3cab28d22c44175d9f84b79d.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import './components/alert/index.js';
+import './components/alert/index.js';
 import './components/utility-header/index.js';
-// import './components/branding/index.js';
-// import './components/navigation/index.js';
+import './components/branding/index.js';
+import './components/navigation/index.js';
